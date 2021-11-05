4bcef253e26f1ec8f583fd264f419f1628c2569d
JJ Jiang <jingjiejiang85@gmail.com>
2021-11-05 14:56:32 +0800

    Test Reflection

---
+++ RefelectionTest/CarLibrary/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 
+ * @author jingjiejiang
+ * @history Nov 5, 2021
+ * 
+ */
+namespace CarLibrary
+{
+    public class Car : IEnumerable
+    {
+        private string name = string.Empty;
+        private string prvtVariable = "PrivateVariable";
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RefelectionTest/RefelectionTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using CarLibrary;
+
+/**
+ * 
+ * @author jingjiejiang
+ * @history Nov 5, 2021
+ * 1. test reflection
+ * https://www.c-sharpcorner.com/blogs/setting-and-getting-private-variable-of-a-class-without-properties
+ * 
+ */
+namespace RefelectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car c = new Car();
+            Type typ = typeof(Car);
+            FieldInfo type = typ.GetField("prvtVariable", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var value = type.GetValue(c);
+            Console.WriteLine(value);
+        }
+    }
+}
