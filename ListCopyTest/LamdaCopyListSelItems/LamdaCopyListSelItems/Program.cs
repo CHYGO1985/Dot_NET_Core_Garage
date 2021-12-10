0ddf95c5c50b26ddf788c435811c2f3d91f26991
Jingjie Jiang <jjiang@travix.com>
2021-12-10 15:34:28 +0800

    Copy Selected List Item via using lamda expression

---
+++ ListCopyTest/LamdaCopyListSelItems/LamdaCopyListSelItems/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//List<int> newNumList = newNumList.Select(num =>
+//{
+    
+//})
+
+//Conskole.WriteLine(a);
+
+//Console.WriteLine("Hello, World!");
+
+
+namespace Application
+{
+    public class Test1
+    {
+        public int Num1 { get; set; }
+
+        //public Test1(int num)
+        //{
+        //    Num1 = num;
+        //}
+    }
+
+    public class Test2
+    {
+        public int Num2 { get; set; }
+
+        //public Test2(int num)
+        //{
+        //    Num2 = num;
+        //}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Test1> numsList = new List<Test1>
+            {
+                new Test1(){ Num1 = 1 },
+                new Test1(){ Num1 = 2 },
+                new Test1(){ Num1 = 3}
+            };
+
+            List<Test2> numsList2 = numsList.Select(num => new Test2()
+            {
+                Num2 = num.Num1
+            }).ToList();
+
+            numsList2.ForEach(num2 => Console.WriteLine("{0}\t", num2.Num2));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
